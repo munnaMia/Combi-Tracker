1c521336751b36643e2624a6effa746e7924b97e
Md Munna Mia <mdmunnamia987@gmail.com>
2025-03-14 20:01:17 +0600

    added validate args method

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,1 +1,15 @@
 package utils
+
+import "errors"
+
+func validateArgs(arg string, args []string) (string, error) {
+	// Running a loop over all commands
+	for _, argValue := range args {
+		if arg == argValue {
+			// If it's a valid command from the command list then retrun the command
+			return arg, nil
+		}
+	}
+	// Else retrun a error that the user command is not exist
+	return "", errors.New("Enter a valid argument!")
+}
