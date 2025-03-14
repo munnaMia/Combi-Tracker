96aa3cd06db774c3c40050fa13d5dd88816112d0
Md Munna Mia <mdmunnamia987@gmail.com>
2025-03-14 20:51:53 +0600

    update the validateargs method

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,15 +1,25 @@
 package utils
 
-import "errors"
+import (
+	"errors"
+	"log"
+	"os"
+	"slices"
+)
 
-func validateArgs(arg string, args []string) (string, error) {
-	// Running a loop over all commands
-	for _, argValue := range args {
-		if arg == argValue {
-			// If it's a valid command from the command list then retrun the command
-			return arg, nil
-		}
+func ValidateArgs(arg []string, args []string) ([]string, error) {
+
+	// Close the program if user doesn't provide any commnads
+	if len(arg) < 1 {
+		log.Print("Usage: combi-tracker <command> [arguments]")
+		os.Exit(1)
 	}
+
+	// If it's a valid command from the command list then retrun the command
+	if slices.Contains(args, arg[1]) {
+		return arg[1:], nil
+	}
+
 	// Else retrun a error that the user command is not exist
-	return "", errors.New("Enter a valid argument!")
+	return nil, errors.New("enter a valid argument")
 }
