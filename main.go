20a37aa88468d17e6ec6fd0953508a64f50aa9ec
Md Munna Mia <mdmunnamia987@gmail.com>
2025-03-14 20:52:49 +0600

    added command and sub commands

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main(){
-	// Main code
+import (
+	"fmt"
+	"os"
+
+	"github.com/munnaMia/Combi-Tracker/cmd"
+	"github.com/munnaMia/Combi-Tracker/internal/utils"
+)
+
+func main() {
+	app := &cmd.Application{
+		Commands: []string{"add", "update", "delete" ,"mark-in-progress", "mark-done", "list"},
+		SubCommands: []string{"done", "todo", "in-progress"},
+	}
+
+
+	// Taking the command line args without progs
+	argsWithoutProg, _ := utils.ValidateArgs(os.Args, app.Commands)
+	fmt.Println(argsWithoutProg)
+
 }
