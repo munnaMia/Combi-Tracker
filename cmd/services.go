a4325096a7f68cec713145b51207886fb5859b22
Md Munna Mia <mdmunnamia987@gmail.com>
2025-04-04 21:33:30 +0600

    update add flag

--- cmd/services.go
+++ cmd/services.go
@@ -1,22 +1,40 @@
 package cmd
 
 import (
+	"time"
+
 	datamodel "github.com/munnaMia/Combi-Tracker/Model"
 	"github.com/munnaMia/Combi-Tracker/internal/utils"
 )
 
 type Application struct {
-	dataModel   datamodel.Model
 	Commands    []string
 	SubCommands []string
 	TodoDb      string
-	PendDb      string
+	ProgDb      string
 	DoneDb      string
 }
 
-// Add a task
-func (app *Application) Add(argsArray []string) {
-
+// Adding a task
+func (app *Application) Add(argsArray []string, todoDbPath string) {
 	taskDiscription := utils.ConvertArrayToString(argsArray[1:]) // taking the task discription and remove the args
+
+	// i have to find the current ID for a new task
+	todoArray := utils.ReadJson(todoDbPath) // pending work list
+
+	// Create a new task
+	newTask := datamodel.Model{
+		Id:          len(todoArray),
+		Description: taskDiscription,
+		Status:      "todo",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   nil,
+	}
+
+	// appending the new task to the pending tasks array
+	todoArray = append(todoArray, newTask)
+
+	// Write into the todo JSON file or database
+	utils.WriteJson(todoDbPath, todoArray)
 
 }
