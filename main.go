0ddac8cf4ac0ecabe57489b60f14c8ae66616a13
Md Munna Mia <mdmunnamia987@gmail.com>
2025-04-04 21:34:48 +0600

    update main file

--- main.go
+++ main.go
@@ -11,23 +11,22 @@ func main() {
 	app := &cmd.Application{
 		Commands:    []string{"add", "update", "delete", "mark-in-progress", "mark-done", "list"},
 		SubCommands: []string{"done", "todo", "in-progress"},
-		TodoDb: "internal/database/todoDb.json",
-		PendDb: "internal/database/pendingDb.json",
-		DoneDb: "internal/database/doneDb.json",
-		
+		TodoDb:      "internal/database/todoDb.json",
+		ProgDb:      "internal/database/progDb.json",
+		DoneDb:      "internal/database/doneDb.json",
 	}
-	
+
 	argsArray, err := utils.ValidateArgs(os.Args, app.Commands) // validating the arguments. and storing all the arguments in a slice.
-	utils.HandleError(err) // If any error occur program will be shutdown
+	utils.HandleError(err)                                      // If any error occur program will be shutdown
 
 	// Creating all the nessary files and directory.
 	utils.CreateFileIfNotExist(app.TodoDb)
-	utils.CreateFileIfNotExist(app.PendDb)
+	utils.CreateFileIfNotExist(app.ProgDb)
 	utils.CreateFileIfNotExist(app.DoneDb)
 
-		switch argsArray[0] {
+	switch argsArray[0] {
 	case "add":
-		app.Add(argsArray) // Adding a task to JSON
+		app.Add(argsArray, app.TodoDb) // Adding a task to JSON
 	}
 
 }
