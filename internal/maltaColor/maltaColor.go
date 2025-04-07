1a9b3debc8dc54b03f8c470077205e16ba2c2979
Md Munna Mia <mdmunnamia987@gmail.com>
2025-04-07 20:21:53 +0600

    adding color pkg for more color options

---
+++ internal/maltaColor/maltaColor.go
@@ -0,0 +1,24 @@
+package maltacolor
+
+const (
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+	Gray    = "\033[37m"
+	White   = "\033[97m"
+)
+
+const (
+	BrightRed     = "\033[31;1m"
+	BrightGreen   = "\033[32;1m"
+	BrightYellow  = "\033[33;1m"
+	BrightBlue    = "\033[34;1m"
+	BrightMagenta = "\033[35;1m"
+	BrightCyan    = "\033[36;1m"
+	BrightGray    = "\033[37;1m"
+	BrightWhite   = "\033[97;1m"
+)
